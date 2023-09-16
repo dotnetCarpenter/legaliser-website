65e53a5325d2e46b0d502952ae8cf595816f5da7
dotnetCarpenter <jon.ronnenberg@gmail.com>
2023-09-16 20:20:57 +0200

    added sitemap - now we can poke google

--- legaliser-website/astro.config.mjs
+++ legaliser-website/astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
 
-export default defineConfig ({
-    site: 'https://legaliser.dk'
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://legaliser.dk',
+  integrations: [sitemap()]
 });
